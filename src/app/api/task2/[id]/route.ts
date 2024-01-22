37c8ba198e6ea2ffb5741f5a8041932ad3ecb1c8
ashraffurqan <132605717+ashraffurqan@users.noreply.github.com>
2024-01-23 02:22:25 +0500

    del

---
+++ src/app/api/task2/[id]/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { user_table, db, user_table2 } from '@/app/lib/drizzle';
+import { eq } from 'drizzle-orm';
+export const DELETE= async(request: Request, {params}:{params:{id:number}})=> {
+    const id = params.id;
+    
+  // console.log(id);
+  
+   const result= await db.delete(user_table2).where(eq(user_table2.id, id))
+   .returning({id:user_table2.id});
+    return NextResponse.json({message:"Get handleew",data:result})
+  }
