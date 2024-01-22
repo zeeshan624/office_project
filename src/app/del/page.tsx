319793ca307b2e7ff1305945db3a339d2aab490b
ashraffurqan <132605717+ashraffurqan@users.noreply.github.com>
2024-01-23 03:00:47 +0500

    delete re

--- src/app/del/page.tsx
+++ src/app/del/page.tsx
@@ -1,45 +1,47 @@
-// "use client"
-// import { useState } from "react";
+"use client"
+import { useState } from "react";
 
-// const Page = () => {
-//   const [id, setID] = useState('');
+const Page = () => {
+  const [id, setID] = useState('');
 
-//   const handleDelete = async () => {
-//     try {
-//       if (!id) {
-//         console.error('User ID is missing.');
-//         return;
-//       }
+  const handleDelete = async () => {
+    try {
+      if (!id) {
+        console.error('User ID is missing.');
+        return;
+      }
 
-//       await fetch(`http://localhost:3000/del?id=${id}`, {
-//         method: 'DELETE',
-//         headers: {
-//           'Content-type': 'application/json; charset=UTF-8',
-//         },
-//       });
+      console.log('Deleting user with ID:', id);
 
-//       console.log('User deleted successfully');
-//       // Add any additional logic or UI updates after successful deletion
-//     } catch (error) {
-//       console.error('Error deleting user:', error);
-//     }
-//   };
+      await fetch(`http://localhost:3000/api/task2/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
 
-//   return (
-//     <div>
-//       <div className='grid grid-cols-6 gap-3 bg-green-500'>
-//         {/* Display the serial number */}
-//         <input
-//           type='text'
-//           placeholder='User ID'
-//           className='p-3 bg-green-300 text-lg rounded-lg'
-//           value={id}
-//           onChange={(e) => setID(e.target.value)}
-//         />
-//       </div>
-//       <button onClick={handleDelete}>Delete User</button>
-//     </div>
-//   );
-// };
+      console.log('User deleted successfully');
+      // Add any additional logic or UI updates after successful deletion
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  };
 
-// export default Page;
+  return (
+    <div>
+      <div className='grid grid-cols-6 gap-3 bg-green-500'>
+        {/* Display the serial number */}
+        <input
+          type='text'
+          placeholder='User ID'
+          className='p-3 bg-green-300 text-lg rounded-lg'
+          value={id}
+          onChange={(e) => setID(e.target.value)}
+        />
+      </div>
+      <button onClick={handleDelete}>Delete User</button>
+    </div>
+  );
+};
+
+export default Page;
